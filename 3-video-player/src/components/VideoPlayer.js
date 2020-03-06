73484b3f2bfa704ff441bf82a47310642a0a4142
Luciana Caraça <luticar@gmail.com>
2020-03-06 22:35:01 +0000

    Create and Style component for UI

--- 3-video-player/src/components/VideoPlayer.js
+++ 3-video-player/src/components/VideoPlayer.js
@@ -1,46 +1,57 @@
 import React, { Fragment, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import poster from "../img/freepik-poster.jpg";
+import video from "../videos/gone.mp4";
 
 const VideoPlayer = props => {
   return (
     <Fragment>
       <div className="container">
-        <p>Custom Video Player</p>
-        <video
-          src="../videos/gone.mp4"
-          id="video"
-          className="screen"
-          poster="../img/poster.png"
-        ></video>
-        <div className="controls">
-          <FontAwesomeIcon
-            icon={["fas", "play"]}
-            className="controls"
-            id="play"
-          />
-          <FontAwesomeIcon
-            icon={["fas", "pause"]}
-            className="controls"
-            id="pause"
-          />
-          <FontAwesomeIcon
-            icon={["fas", "stop"]}
-            className="controls"
-            id="stop"
-          />
+        <h1>Custom Video Player</h1>
+        <div className="player">
+          <video
+            src={video}
+            id="video"
+            className="screen"
+            poster={poster}
+          ></video>
         </div>
-        <input
-          type="range"
-          className="progress"
-          id="progress"
-          min="0"
-          max="100"
-          step="0.1"
-          value="0"
-        />
-        <span className="timestamp" id="timestamp">
-          00:00
-        </span>
+        <div className="controls">
+          <button className="btn">
+            <FontAwesomeIcon
+              icon={["fas", "play"]}
+              className="play"
+              id="play"
+            />
+          </button>
+          <button className="btn">
+            <FontAwesomeIcon
+              icon={["fas", "pause"]}
+              className="pause"
+              id="pause"
+            />{" "}
+          </button>
+          <button className="btn">
+            <FontAwesomeIcon
+              icon={["fas", "stop"]}
+              className="stop"
+              id="stop"
+            />
+          </button>
+
+          <input
+            type="range"
+            className="progress"
+            id="progress"
+            min="0"
+            max="100"
+            step="0.1"
+            value="0"
+          />
+          <span className="timestamp" id="timestamp">
+            00:00
+          </span>
+        </div>
       </div>
     </Fragment>
   );
