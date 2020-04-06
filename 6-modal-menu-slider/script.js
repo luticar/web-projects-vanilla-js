8c6e172a6b3e27123dd1479107c83433f4ed9504
Luciana Caraça <luticar@gmail.com>
2020-04-06 17:06:38 +0100

    Use DOM content load event

--- 6-modal-menu-slider/script.js
+++ 6-modal-menu-slider/script.js
@@ -1,22 +1,23 @@
-window.addEventListener("DOMContentLoaded", event => {
-  console.log("DOM fully loaded and parsed");
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("page loaded!");
+
+  const toggle = document.getElementById("toggle");
+  const openModal = document.getElementById("open");
+  const closeModal = document.getElementById("close");
+  const modal = document.getElementById("modal");
+
+  toggle.addEventListener("click", () =>
+    document.body.classList.toggle("show-nav")
+  );
+
+  openModal.addEventListener("click", () => modal.classList.add("show-modal"));
+  closeModal.addEventListener("click", () =>
+    modal.classList.remove("show-modal")
+  );
+
+  // close modal when clicking in the area outside it
+
+  window.addEventListener("click", e =>
+    e.target == modal ? modal.classList.remove("show-modal") : false
+  );
 });
-
-const toggle = document.getElementById("toggle");
-const openModal = document.getElementById("open");
-const closeModal = document.getElementById("close");
-const modal = document.getElementById("modal");
-
-toggle.addEventListener("click", () =>
-  document.body.classList.toggle("show-nav")
-);
-
-openModal.addEventListener("click", () => modal.classList.add("show-modal"));
-closeModal.addEventListener("click", () =>
-  modal.classList.remove("show-modal")
-);
-
-// close modal when clicking in the area outside it
-window.addEventListener("click", e =>
-  e.target == modal ? modal.classList.remove("show-modal") : false
-);
